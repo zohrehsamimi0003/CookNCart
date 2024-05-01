f94d97a9069b971b76dbf75b2c03203976f75cc6
Zaskia <zaskia.de_chazal0001@stud.hkr.se>
2024-05-01 18:10:48 +0200

    Database update

--- Database.py
+++ Database.py
@@ -8,7 +8,8 @@ class Database:
             password="testpw",
             database="cookncart"
         )
-        
+
+    #CHECKS IF THE LOGIN DETAILS RETURNS A USER AND SO EXISTS    
     def login_validation(self, email, password):
         try:
             my_cursor = self.db.cursor()
@@ -19,19 +20,45 @@ class Database:
         except mysql.connector.Error as e:
             print(f"Database Error: {e}")
             return False  # Return False only if there's an error
+
+    #INSERTS A USER BEING CREATED INTO THE DATABASE    
+    def insert_user(self, username_entry, email_entry, password_entry, diet_type): 
+        mycursor = self.db.cursor()
+        query = '''SELECT DietTypeId INTO @DietTypeId FROM diet_types WHERE DietType = 'Vegetarian';
+                INSERT INTO users (Username, UserPassword, Email, DietType)
+                VALUES (%s, %s, %s, %s)'''
         
-    def insert_user(self, user): #pass a user object here ()
-        pass
+        parameters = (username_entry, password_entry, email_entry, diet_type)
+        mycursor.execute(query, parameters)
 
-    def delete_user(self, email):
-        pass
+        return mycursor.rowcount #will return 1 if properly inserted, maybe use that to check and instantiate user object?
 
-    def update_user(self, user): #pass user object
-        pass
+    #DELETES AN EXISTING USER
+    def delete_user(self, user):
+        mycursor = self.db.cursor()
+        query = '''DELETE FROM users WHERE Email = %s;'''
+        mycursor.execute(query, (user.email,))
+
+    #UPDATES THE PASSWORD OF AN EXISTING USER
+    def update_password(self, user, new_password): #pass user object here and the new password from the entry box
+        mycursor = self.db.cursor()
+        query = '''UPDATE users SET UserPassword = % WHERE Email = %;'''
+        parameters = (new_password, user.email)
+        mycursor.execute(query, parameters)
+        mycursor.rowcount
+
+    #UPDATES THE DIET TYPE OF AN EXISTING USER
+    def update_diet_type(self, user, new_diet_type): #pass user object here and the new password from the entry box
+        mycursor = self.db.cursor()
+        query = '''SELECT DietTypeId INTO @DietTypeId FROM diet_types WHERE DietType = %s;
+        UPDATE users SET DietTypeId = @DietTypeId WHERE Email = %s;'''
+        parameters = (new_diet_type, user.email)
+        mycursor.execute(query, parameters)
+        mycursor.rowcount
 
 
 
-    #based on time of day
+    #SELECTS A RANDOM RECIPE BASED ON LUNCH TYPE (USED FOR TIME OF DAY RECIPE)
     def get_random_recipe(self, meal_time): 
         #implement a function in your code that decides if 
         #its time for a random lunch/breankfast/dinner recipe
