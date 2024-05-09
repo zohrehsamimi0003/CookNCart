76357b3a52c16359712103face40aac89548dbfc
Anitta Antony <anittaantonyal>
2024-05-09 23:31:36 +0200

    anitta

--- welcome_screen.py
+++ welcome_screen.py
@@ -1,5 +1,5 @@
 
-import tkinter
+import tkinter 
 import meal_planner
 import timed_recipe
 import search_recipe
@@ -7,40 +7,17 @@ import helpers
 import start_app
 from datetime import datetime
 import session
+from main_window import MainWindow
 
 class WelcomeScreen:
     def __init__(self, session):
         self.session = session
         self.main_win = session.main_win
         self.my_db = session.my_db
-        self.create_widgets()    
+        self.create_widgets()
+         
 
-    def create_widgets(self):
-
-        self.frame = tkinter.Frame(self.main_win.root, width=1000, height=1000, bg='#F9EBEA')
-        CookNCart = tkinter.Button(
-            self.frame, text="CookNCart", bg='#D2B4DE', font=("Comic Sans MS", 25))
-        Meal_planner_label = tkinter.Button(
-            self.frame, text="Meal Planner", bg='#D2B4DE', font=("Georgia", 12), command=self.meal_planner_button_clicked)
-        Search_recipe_label = tkinter.Button(
-            self.frame, text="Search Recipe", bg='#D2B4DE', font=("Georgia", 12), command=self.search_recipe_button_clicked)
-        Timed_recipe_label = tkinter.Button(
-            self.frame, text="Timed Recipe", bg='#D2B4DE', font=("Georgia", 12), command=self.timed_recipe_button_clicked)
-        log_off_button = tkinter.Button(
-            self.frame, text="Log Off", bg='#F5B7B1', font=("Georgia", 11), command=self.log_off_button_clicked)
-        profile_button = tkinter.Button(
-            self.frame, text="Profile", bg='#F5B7B1', font=("Georgia", 11), command=self.profile_button_clicked)
-
-        CookNCart.grid(row=0, column=2, columnspan=3, sticky="ne", padx=20, pady=20)
-        Meal_planner_label.grid(row=1, column=0, columnspan=3, pady=20, ipadx=50, ipady=20)
-        Search_recipe_label.grid(row=2, column=0, columnspan=3, pady=20, ipadx=50, ipady=20)
-        Timed_recipe_label.grid(row=3, column=0, columnspan=3, pady=20, ipadx=50, ipady=20)
-        log_off_button.grid(row=0, column=1, sticky="ne", padx=10, pady=20)
-        profile_button.grid(row=0, column=2, sticky="ne", padx=10, pady=(20,10))
-        self.frame.columnconfigure(0, weight=1)
-        self.frame.columnconfigure(1, weight=1)
-        self.frame.pack(fill=tkinter.BOTH, expand=True, side='top')
-
+    
     def meal_planner_button_clicked(self):
        self.frame.destroy()
        meal_planner.MealPlanner(self.session)
@@ -72,4 +49,65 @@ class WelcomeScreen:
 
     def log_off_button_clicked(self):
         helpers.log_off_btn_screen_change(self.frame, self.session)
+    def back_button_clicked(self):
+        pass
+    
+    def create_widgets(self):
         
+        self.frame = tkinter.Frame(self.main_win.root, width=1280, height=720, bg='#F9EBEA')
+        self.frame.place(x=0, y=0)
+        button_style = {
+            'font': ("Algerian", 16),
+            'bg': 'tan4',
+            'fg': 'black',
+            'padx': 40,
+            'pady': 10,
+            'width': 15,
+            'relief': tkinter.RAISED,
+            'borderwidth': 6
+        }
+
+
+        Meal_planner_label = tkinter.Button(
+            self.frame, text="Meal Planner", **button_style, command=self.meal_planner_button_clicked)
+        Search_recipe_label = tkinter.Button(
+            self.frame, text="Search Recipe", **button_style, command=self.search_recipe_button_clicked)
+        Timed_recipe_label = tkinter.Button(
+            self.frame, text="Timed Recipe", **button_style, command=self.timed_recipe_button_clicked)
+        log_off_button = tkinter.Button(
+            self.frame, text="Log Off", bg='#8b5a2b',fg='white', font=("Georgia", 11), borderwidth=1,command=self.log_off_button_clicked)
+        profile_button = tkinter.Button(
+            self.frame, text="Profile", bg='#8b5a2b',fg='white', font=("Georgia", 11), borderwidth=1, command=self.profile_button_clicked)
+        back_button = tkinter.Button(self.frame, text="Back", bg='#8b5a2b', fg='white', command=self.back_button_clicked,  borderwidth=1)
+
+        # Position the buttons and labels
+        Meal_planner_label.grid(row=2, column=0, pady=(100,20), padx=35, ipadx=50, ipady=20, sticky='w')
+        Search_recipe_label.grid(row=3, column=0, pady=20, padx=35, ipadx=50, ipady=20, sticky='w')
+        Timed_recipe_label.grid(row=4, column=0, pady=20, padx=35, ipadx=50, ipady=20, sticky='w')
+        # Create descriptive labels next to each button
+        description_label_mp = tkinter.Label(self.frame, text="Plan your meals for the week here.", bg='#F9EBEA', font=("Verdana", 12), justify='left', width=40)
+        description_label_sr = tkinter.Label(self.frame, text="Best match for your ingredients.", bg='#F9EBEA', font=("Verdana", 12), justify='left', width=40)
+        description_label_tr = tkinter.Label(self.frame, text="Let's cook for Lunch/Dinner?", bg='#F9EBEA', font=("Verdana", 12), justify='left', width=40)
+
+        
+        log_off_button.grid(row=0, column=1, sticky="ne", padx=10, pady=(0,20), ipadx=20,ipady=10)
+        profile_button.grid(row=0, column=2, sticky="ne", padx=10, pady=(0,20), ipadx=20, ipady=10)
+        back_button.grid(row=5, column=2, sticky="se", padx=10, pady=(0,20), ipadx=20, ipady=10)
+        description_label_mp.grid(row=2, column=1, padx=10, pady=(10,20), sticky='w')
+        description_label_sr.grid(row=3, column=1, padx=10, pady=20, sticky='w')
+        description_label_tr.grid(row=4, column=1, padx=10, pady=20, sticky='w')
+
+
+        self.frame.pack(fill=tkinter.BOTH, expand=True, side='top')
+
+        self.frame.columnconfigure(0, weight=1)
+        self.frame.columnconfigure(1, weight=1)
+
+        # Pack the frame
+        self.main_win.logo_label.lift()
+
+    
+        # Pack the frame
+        self.frame.pack(fill=tkinter.BOTH, expand=True, anchor='n', padx=0, pady=0)
+
+        self.main_win.root.mainloop()
