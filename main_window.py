c7cd209dd4a4dd4ee00a5bddf6b6226374c86b8a
Zaskia <zaskia.de_chazal0001@stud.hkr.se>
2024-05-10 08:28:53 +0200

    updated where bg colour is set and made border smaller

--- main_window.py
+++ main_window.py
@@ -6,16 +6,17 @@ class MainWindow():
 
     def __init__(self, root):
         self.root = root
+        self.bg = '#fcf2dc'
         root.title("CookNCart")
         root.geometry('1000x1000')
-        root.configure(bg='#F9EBEA')
+        root.configure(bg=self.bg)
 
         # Load and resize the logo image
         self.logo = Image.open("Images\logo.png")  # Update to your logo file path
         self.logo = ImageTk.PhotoImage(self.logo.resize((300, 100)))
 
         # Create the button with a matching background color and slight border
-        self.logo_label = tkinter.Label(root, image=self.logo, width=300, height=100, bg='#F9EBEA', borderwidth=1, relief=tkinter.FLAT)
+        self.logo_label = tkinter.Label(root, image=self.logo, width=300, height=100, bg=self.bg, borderwidth=1, relief=tkinter.FLAT)
         self.logo_label.place(x=0, y=0)
 
 
